300a4b7f882947b896b125292e6676712f156db8
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2023-04-27 14:17:01 +0300

    many threads

--- src/Program.java
+++ src/Program.java
@@ -24,7 +24,8 @@
         public static void main(String[] args) {
 
             System.out.println("Main thread started...");
-            new JThread("JThread").start();
+            for(int i=1; i < 6; i++)
+                new JThread("JThread " + i).start();
             System.out.println("Main thread finished...");
         }
     }
