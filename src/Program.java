a0465958656fd6ea7d4fdc4dd5473f1d630c6964
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2023-04-27 15:02:55 +0300

    from lambda

--- src/Program.java
+++ src/Program.java
@@ -1,29 +1,23 @@
 
 
 
-//Другой способ определения потока представляет реализация интерфейса Runnable. Этот интерфейс имеет один метод run:
-        class MyThread implements Runnable {
+public class Program {
 
+    public static void main(String[] args) {
 
-            public void run() {
-
-                System.out.printf("%s started... \n", Thread.currentThread().getName());
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread has been interrupted");
-                }
-                System.out.printf("%s finished... \n", Thread.currentThread().getName());
+        System.out.println("Main thread started...");
+        Runnable r = ()->{
+            System.out.printf("%s started... \n", Thread.currentThread().getName());
+            try{
+                Thread.sleep(500);
             }
-        }
-
-        public class Program {
-
-            public static void main(String[] args) {
-
-                System.out.println("Main thread started...");
-                Thread myThread = new Thread(new MyThread(), "MyThread");
-                myThread.start();
-                System.out.println("Main thread finished...");
+            catch(InterruptedException e){
+                System.out.println("Thread has been interrupted");
             }
-        }
+            System.out.printf("%s finished... \n", Thread.currentThread().getName());
+        };
+        Thread myThread = new Thread(r,"MyThread");
+        myThread.start();
+        System.out.println("Main thread finished...");
+    }
+}
