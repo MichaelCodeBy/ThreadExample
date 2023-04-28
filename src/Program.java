1d87933afc3587729b861eee70b9db33b431e99a
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2023-04-28 09:22:20 +0300

    Phaser

--- src/Program.java
+++ src/Program.java
@@ -1,58 +1,70 @@
 
-//Класс Exchanger предназначен для обмена данными между потоками. Он является типизированным и
-// типизируется типом данных, которыми потоки должны обмениваться.
+//Класс Phaser позволяет синхронизировать потоки, представляющие отдельную фазу или стадию выполнения общего действия.
+// Phaser определяет объект синхронизации, который ждет, пока не завершится определенная фаза.
+// Затем Phaser переходит к следующей стадии или фазе и снова ожидает ее завершения.
 
-import java.util.concurrent.Exchanger;
+//Основные методы класса Phaser:
+//
+//int register(): регистрирует участника, который выполняет фазы, и возвращает номер текущей фазы - обычно фаза 0
+//
+//int arrive(): сообщает, что участник завершил фазу, и возвращает номер текущей фазы
+//
+//int arriveAndAwaitAdvance(): аналогичен методу arrive, только при этом заставляет phaser ожидать
+// завершения фазы всеми остальными участниками
+//
+//int arriveAndDeregister(): сообщает о завершении всех фаз участником и снимает его с регистрации.
+// Возвращает номер текущей фазы или отрицательное число, если синхронизатор Phaser завершил свою работу
+//
+//int getPhase(): возвращает номер текущей фазы
+
+import java.util.concurrent.Phaser;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Exchanger<String> ex = new Exchanger<String>();
-        new Thread(new PutThread(ex)).start();
-        new Thread(new GetThread(ex)).start();
+        Phaser phaser = new Phaser(1);
+        new Thread(new PhaseThread(phaser, "PhaseThread 1")).start();
+        new Thread(new PhaseThread(phaser, "PhaseThread 2")).start();
+
+        // ждем завершения фазы 0
+        int phase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Фаза " + phase + " завершена");
+        // ждем завершения фазы 1
+        phase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Фаза " + phase + " завершена");
+
+        // ждем завершения фазы 2
+        phase = phaser.getPhase();
+        phaser.arriveAndAwaitAdvance();
+        System.out.println("Фаза " + phase + " завершена");
+
+        phaser.arriveAndDeregister();
     }
 }
 
-class PutThread implements Runnable{
+class PhaseThread implements Runnable{
 
-    Exchanger<String> exchanger;
-    String message;
+    Phaser phaser;
+    String name;
 
-    PutThread(Exchanger<String> ex){
+    PhaseThread(Phaser p, String n){
 
-        this.exchanger=ex;
-        message = "Hello Java!";
+        this.phaser=p;
+        this.name=n;
+        phaser.register();
     }
     public void run(){
 
-        try{
-            message=exchanger.exchange(message);//Обмен данными производится с помощью единственного метода этого класса exchange()
-            System.out.println("PutThread has received: " + message);
-        }
-        catch(InterruptedException ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-}
-class GetThread implements Runnable{
+        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+        phaser.arriveAndAwaitAdvance(); // сообщаем, что первая фаза достигнута
 
-    Exchanger<String> exchanger;
-    String message;
+        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+        phaser.arriveAndAwaitAdvance(); // сообщаем, что вторая фаза достигнута
 
-    GetThread(Exchanger<String> ex){
-
-        this.exchanger=ex;
-        message = "Hello World!";
-    }
-    public void run(){
-
-        try{
-            message=exchanger.exchange(message);
-            System.out.println("GetThread has received: " + message);
-        }
-        catch(InterruptedException ex){
-            System.out.println(ex.getMessage());
-        }
+        System.out.println(name + " выполняет фазу " + phaser.getPhase());
+        phaser.arriveAndDeregister(); // сообщаем о завершении фаз и удаляем с регистрации объекты
     }
 }
