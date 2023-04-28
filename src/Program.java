b960cade592d4fdb48c2ccc527dbcfec519107a6
MichaelCodeBy <93823188+MichaelCodeBy@users.noreply.github.com>
2023-04-28 09:25:03 +0300

    Phaser with sleep method

--- src/Program.java
+++ src/Program.java
@@ -60,10 +60,21 @@ class PhaseThread implements Runnable{
 
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndAwaitAdvance(); // сообщаем, что первая фаза достигнута
+        try{
+            Thread.sleep(200);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndAwaitAdvance(); // сообщаем, что вторая фаза достигнута
-
+        try{
+            Thread.sleep(200);
+        }
+        catch(InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
         System.out.println(name + " выполняет фазу " + phaser.getPhase());
         phaser.arriveAndDeregister(); // сообщаем о завершении фаз и удаляем с регистрации объекты
     }
