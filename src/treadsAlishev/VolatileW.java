95f1de7cf366192f79049a475602d48255691133
Michael <gomellc@gmail.com>
2023-05-05 11:54:17 +0300

    keyword volatile

---
+++ src/treadsAlishev/VolatileW.java
@@ -0,0 +1,36 @@
+package treadsAlishev;
+
+import java.util.Scanner;
+
+public class VolatileW {
+    public static void main(String[] args) {
+
+        MyThread1 myThread = new MyThread1();
+        myThread.start();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
+
+
+    }
+}
+
+class MyThread1 extends Thread {
+    private volatile boolean running = true;// volatile переменная может быть изменена, не кешировать
+//volatile убирает когерентность кешей
+    public void run() {
+        while (running) {
+            System.out.println("Hello!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
+    }
+}
