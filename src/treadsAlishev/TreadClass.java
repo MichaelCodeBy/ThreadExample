bc2d46ddd64fb12e784a64e1f623482767334d8b
Michael <gomellc@gmail.com>
2023-05-05 10:35:07 +0300

    treads from ALISHEV. implements interface runnable

--- src/treadsAlishev/TreadClass.java
+++ src/treadsAlishev/TreadClass.java
@@ -1,16 +1,42 @@
 package treadsAlishev;
 
 public class TreadClass {
-    public static void main(String[] args) { //главный поток main
-        MyThread myThread = new MyThread();// build new object
+    public static void main(String[] args) throws InterruptedException { //главный поток main
+        Thread.sleep(300);
+       /* MyThread myThread = new MyThread();// build new object
         myThread.start(); // метод start создает новый поток и выполняет то что написано в методе run
+
+        MyThread myThread2 = new MyThread();// build second object
+        myThread2.start();// start second tread
         System.out.println("Hello world");
+   */
+        Thread thread = new Thread(new Runner());
+        thread.start();
+        System.out.println("Hello from main thread");
+    }
+}
+class Runner implements Runnable {//реализуем интерфейс раннабл
+    @Override
+    public void run(){
+        for (int i=1; i<10; i++){
+            try {
+                Thread.sleep(100); // sleep
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello from my Thread"+i);
+        }
     }
 }
 class MyThread extends Thread{
     public void run(){ //переопределили метод ран в этом классе
-        for (int i=1; i<48; i++){
-            System.out.println("Hello from my Thread");
+        for (int i=1; i<100; i++){
+            try {
+                Thread.sleep(100); // sleep
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello from my Thread"+i);
         }
     }
 }
