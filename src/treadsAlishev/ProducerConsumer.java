c2dba2b973c63c9e07ec68eceb50ad37284cb824
Michael <gomellc@gmail.com>
2023-05-10 10:29:09 +0300

    pattern producer-consumer

--- src/treadsAlishev/ProducerConsumer.java
+++ src/treadsAlishev/ProducerConsumer.java
@@ -9,17 +9,31 @@ public class ProducerConsumer {
 
     private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10); //статическая переменная для доступа к ней в статических методах
 
-    public static void main(String[] args) {
-Thread thread1 =new Thread(new Runnable() {
-    @Override
-    public void run() {
-        try {
-            produce();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-});
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
     }
 
     private static void produce() throws InterruptedException {
@@ -27,12 +41,13 @@ Thread thread1 =new Thread(new Runnable() {
         while (true) {//бесконечный цикл
             queue.put(random.nextInt()); //получаем случайное целое число элементы в кю
         }
-        private static void consumer () { //потребитель который берет элементы из кю
+    }
+        private static void consumer() throws InterruptedException { //потребитель который берет элементы из кю
             while (true) {
                 Thread.sleep(100);
                 System.out.println(queue.take()); //метод ожидает если в очереди нет элементов
-                System.out.println("queue size is "+ queue.size());
+                System.out.println("queue size is " + queue.size());
             }
         }
     }
-}
+
