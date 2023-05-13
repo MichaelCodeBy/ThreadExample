3c4e2b18ea990d02319c043629dd3745d7a8cb5f
Michael <gomellc@gmail.com>
2023-05-13 10:03:14 +0300

    tread from Runnable

--- src/treadsAlishev/CalableAndFutureClasses.java
+++ src/treadsAlishev/CalableAndFutureClasses.java
@@ -2,21 +2,30 @@ package treadsAlishev;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 // для возврата значений из потоков и выбрасывания исключений из потоков
 public class CalableAndFutureClasses {
-    public static void main(String[] args) {
+    private static int result;
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService= Executors.newFixedThreadPool(1);
-        executorService.submit(()->{
-            System.out.println("Starting");
-            try {
-                Thread.sleep(3000);
-            }catch (InterruptedException e){
-                e.printStackTrace();
+        executorService.submit(new Runnable(){
+            @Override
+            public void run() {
+
+                System.out.println("Starting");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Finished");
+                result=5;
             }
-            System.out.println("Finished");
         });
         executorService.shutdown();
+        executorService.awaitTermination(1,TimeUnit.DAYS);
+        System.out.println(result);
     }
     public static int calculate(){
 
